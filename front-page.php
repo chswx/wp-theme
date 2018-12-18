7cb3a5f6fd62ead666f3676537393bbdb9522b55
Jared Smith <jared@jaredwsmith.com>
2018-12-18 14:35:16 -0500

    Front page

---
+++ front-page.php
@@ -0,0 +1,13 @@
+<?php get_header(); ?>
+<div id="wrapper">
+<h2>CURRENTLY</h2>
+<?php
+if(have_posts()) :
+    while(have_posts()): the_post(); ?>
+        <h1><?php the_title(); ?></h1>
+    <?php endwhile;
+endif;
+?>
+</div>
+<?php
+get_footer();
