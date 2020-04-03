2584811c3431333b5726bcab680abd823c93f5d7
Jared Smith <jared@jaredwsmith.com>
2020-04-03 08:07:03 -0400

    Add RSS feed links to the header

--- functions.php
+++ functions.php
@@ -22,6 +22,7 @@ add_theme_support('title-tag');
 add_theme_support('wp-block-styles');
 add_theme_support('responsive-embeds');
 add_theme_support('post-thumbnails');
+add_theme_support('automatic-feed-links');
 
 /**
  * WordPress' missing is_blog_page() function.  Determines if the currently viewed page is
