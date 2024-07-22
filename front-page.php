049f6be9a50931df8bbf765e8cefae65b6b593ad
Jared Smith <jared@jaredwsmith.com>
2024-07-21 23:29:50 -0400

    Rearranging homepage sections a little

--- front-page.php
+++ front-page.php
@@ -8,14 +8,11 @@ get_header();
 </div>
 <?php do_action('wxpress_alerts'); ?>
 <?php do_action('chswx_updates'); ?>
-<div id="forecast">
-    <h2>Forecast</h2>
-    <?php do_action('wxpress_forecast'); ?>
-</div>
 <?php
 $blog_args = [
     'post_type' => 'post',
     'limit' => 1,
+    'category_name' => 'Forecasts',
     'tax_query' => [[
         'taxonomy' => 'post_format',
         'field'    => 'slug',
@@ -27,8 +24,8 @@ $blog_query = new WP_Query($blog_args);
 if ($blog_query->have_posts()) {
 ?>
     <div id="blog-intro">
-        <h2>Charleston Weather Blog</h2>
-        <p class="intro-text">Forecast explanations, atmospheric science, and other cool weather-related stuff for Charleston, SC</p>
+        <h2>Charleston Weather Discussion</h2>
+        <p class="intro-text">The latest forecast and discussion for the Charleston, SC metro area</p>
         <?php
         $blog_query->the_post();
         get_template_part('template-parts/part', 'post'); ?>
@@ -37,5 +34,9 @@ if ($blog_query->have_posts()) {
             wp_reset_postdata();
         }
             ?>
+<div id="forecast">
+    <h2>Charleston Area <abbr title="National Weather Service">NWS</abbr> Forecast</h2>
+    <?php do_action('wxpress_forecast'); ?>
+</div>
 <?php
 get_footer();
